d44b93d4e2cb69fdd3dfbe9e541a0cb8f93a8a34
Duy Tran <duytran2310@gmail.com>
2022-04-14 21:52:03 -0500

    generate secret key

--- get_secret_key.py
+++ get_secret_key.py
@@ -1,2 +1,4 @@
 from django.core.management.utils import get_random_secret_key
-print(get_random_secret_key())'
+env_file = open('config/.env', 'w')
+env_file.write('SECRET_KEY={}'.format(get_random_secret_key()))
+env_file.close()
