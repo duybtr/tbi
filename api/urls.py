cd299407fb6c45f0fc0c60fa49c8e42f31b0bd94
Duy Tran <duytran2310@gmail.com>
2022-05-07 13:07:07 -0500

    disabled apis for now. Need to have security in place first

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,6 @@ from .views import ExpenseAPIView
 from .views import RevenueAPIView
 
 urlpatterns = [
-    path('expense', ExpenseAPIView.as_view()),
-    path('revenue', RevenueAPIView.as_view()),
-
+    #path('expense', ExpenseAPIView.as_view()),
+    #path('revenue', RevenueAPIView.as_view()),
 ]
