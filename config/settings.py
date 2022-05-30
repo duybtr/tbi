01ad202b1a8bf3cd630363829ca15be0d1fa817c
Duy Tran <duytran2310@gmail.com>
2022-05-29 20:52:25 -0500

    remove htmx

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,6 @@ INSTALLED_APPS = [
     
     #3rd_party
     'rest_framework', #new
-    'django_htmx', #new
     'transactions', #new
     'accounts', #new
     
@@ -93,7 +92,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_htmx.middleware.HtmxMiddleware", #new
 ]
 
 ROOT_URLCONF = 'config.urls'
